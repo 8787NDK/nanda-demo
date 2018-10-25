171cf0da77f93d45687d3f133dc447bf26db9d23
Nanda <8787nanda@gmail.com>
2018-10-25 16:46:57 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ArchiveComponent } from './archive/archive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ArchiveComponent,
+    HomeComponent,
+    ArchiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'archive/:year/:month',component:ArchiveComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
